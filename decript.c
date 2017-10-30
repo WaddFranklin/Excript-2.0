cbcf8d462b948a8a017b5e66824ff0ecd28975f8
Wadd_Franklin <waddinsohn@gmail.com>
2017-10-30 12:30:02 -0300

    Criação das funções para a decifragem da mensagem

---
+++ decript.c
@@ -0,0 +1,22 @@
+#include <math.h>
+
+long int search_inverse_mod (long int e, long int module) {
+
+    if (module == 1) {
+
+        return (long int)(e / module);
+    }
+    else {
+        return search_inverse_mod(module, e % module);
+    }
+}
+
+long int extract_root_mod (long int M, long int d) {
+
+    return pow(M, d);
+}
+
+void decript () {
+
+    // code here
+}
---
+++ decript.h
@@ -0,0 +1,19 @@
+/*
+ * Recebe o expoente "e" e "module" (= (p - 1)*(q - 1)).
+ * Retorna o inverso de "e" módulo "module".
+ */
+long int search_inverse_mod (long int e, long int module);
+
+/*
+ * Recebe o valor do caractere criptografado "M" e o inver-
+ * so do expoente "e" módulo "N". Retorna o valor do ca-
+ * ractere decifrado m dado por M = m ^ (d * e) pela fór-
+ * mula de Euler.
+ */
+long int extract_root_mod (long int M, long int d);
+
+/*
+ * Chama as funções necessárias para decifrar a mensagem
+ * criptografada. ... . Não retorna nada;
+ */
+void decript ();
