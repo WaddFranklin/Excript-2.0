3e5db98d57090d46780c80b8f4dd8e9c57823390
Wadd_Franklin <waddinsohn@gmail.com>
2017-11-01 08:49:15 -0300

    Organizando as funções de decript.c e .h

--- decript.c
+++ decript.c
@@ -1,10 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <math.h>
+
+#define STR_SIZE 100000
 
 long int search_inverse_mod (long int e, long int module) {
 
     if (module == 1) {
 
-        return (long int)(e / module);
+        return ((-1) * (e / module));
     }
     else {
         return search_inverse_mod(module, e % module);
@@ -16,7 +21,44 @@ long int extract_root_mod (long int M, long int d) {
     return pow(M, d);
 }
 
+void print_decrypted_text (char *str, long int d) {
+
+    int i;
+    long int number;
+    int count = 0;
+    char str_aux[3];
+
+    for (i = 0 ; i < strlen(str) ; i++) {
+
+        str_aux[count++] = str[i];
+
+        if (count == 3) {
+
+            number = atoi(str_aux);
+            number = extract_root_mod(number, d);
+            printf("%c", number);
+            count = 0;
+            i++;
+        }
+    }
+
+    printf("\n\n");
+    return;
+}
+
 void decript () {
 
-    // code here
+    char encrypted_text[STR_SIZE];
+    long int p, q, e, d, module;
+
+    // ler o 'p', 'q' e o 'e' de um arquivo
+    module = (p - 1) * (q - 1);
+    d = search_inverse_mod(e, module);
+
+    printf("Digite a mensagem codificada:\n\n > ");
+    scanf("%[^\n]s", encrypted_text);
+    getchar();
+
+    print_decrypted_text(encrypted_text, d);
+    return;
 }
--- decript.h
+++ decript.h
@@ -1,3 +1,11 @@
+
+/*
+ * Exibe na tela a mensagem decifrada. Recebe um ponteiro para
+ * a string com a mensagem criptografada e o
+ * inverso de e módulo (p - 1)*(q - 1). Não retorna nada.
+ */
+void print_decrypted_text (char *str, long int d);
+
 /*
  * Recebe o expoente "e" e "module" (= (p - 1)*(q - 1)).
  * Retorna o inverso de "e" módulo "module".
@@ -6,7 +14,7 @@ long int search_inverse_mod (long int e, long int module);
 
 /*
  * Recebe o valor do caractere criptografado "M" e o inver-
- * so do expoente "e" módulo "N". Retorna o valor do ca-
+ * so do expoente "e" módulo (p - 1)*(q - 1). Retorna o valor do ca-
  * ractere decifrado m dado por M = m ^ (d * e) pela fór-
  * mula de Euler.
  */
@@ -14,6 +22,6 @@ long int extract_root_mod (long int M, long int d);
 
 /*
  * Chama as funções necessárias para decifrar a mensagem
- * criptografada. ... . Não retorna nada;
+ * criptografada. Sem parâmetros. Não retorna nada.
  */
 void decript ();
